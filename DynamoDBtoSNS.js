ef83b1a4fd97a0853c1de0d49ed29eca3c551c48
Jay Shah <54587542+jay1299@users.noreply.github.com>
2023-01-15 18:57:07 +0530

    Update 1

--- DynamoDBtoSNS.js
+++ DynamoDBtoSNS.js
@@ -6,6 +6,6 @@ exports.handler = async (event) => {           //Since SNS calls are async w.r.t
         "Message": JSON.stringify(event.Records[0].dynamodb.NewImage),
         //"Message": JSON.stringify(event.Records[0].dynamodb.OldImage) --> You can use this when new and old
                                                                         //  image is configured in DynamoDB streams
-        "TopicArn": "arn:aws:sns:us-east-1:017435043832:DynamoDBxSNSxLambda"
+        "TopicArn": "Your SNS topic ARN here"
     }).promise();
 };
